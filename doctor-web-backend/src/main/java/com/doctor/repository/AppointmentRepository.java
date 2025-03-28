d36e18825a038df73b33a78930861f98911829f5
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-03-28 15:33:35 +0530

    Delete doctor-web-backend directory

--- doctor-web-backend/src/main/java/com/doctor/repository/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.doctor.repository;
-
-import com.doctor.entity.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
--- doctor-web-backend/src/main/java/com/doctor/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.doctor.repository;
-
-import com.doctor.entity.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
